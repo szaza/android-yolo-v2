ea8e9b40f75d9d4b255c8f3eb8dd67e80523314a
Szabo Zoltan <szabozoltanbors@gmail.com>
2018-01-17 22:03:48 +0200

    License changed from GPL to Apache 2

--- src/org/tensorflow/yolo/util/ImageUtils.java
+++ src/org/tensorflow/yolo/util/ImageUtils.java
@@ -1,18 +1,17 @@
-/*
- * Copyright 2017 The Android Things Samples Authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/* Copyright 2018 The Android YOLOv2 sample application Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package org.tensorflow.yolo.util;
 
 import android.graphics.Matrix;
@@ -20,7 +19,7 @@ import android.graphics.Matrix;
 /**
  * Utility class for manipulating images.
  * Modified by Zoltan Szabo
- * URL: https://github.com/szaza/android-yolov2
+ * URL: https://github.com/szaza/android-yolo-v2
  **/
 public class ImageUtils {
     // This value is 2 ^ 18 - 1, and is used to clamp the RGB values before their ranges
